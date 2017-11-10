d04ca3d09b7fc6b4c9c37ba478ca774c0adde7a7
saowaluck <popsoften@gmail.com>
2017-11-10 23:01:14 +0700

    Add git ignore and remove folder  nodemuldual :panda_face:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('I love Node js')
+const Fizzbuzz = function(number){
+    if( number % 5 == 0 && number % 3 ==0 ){
+        console.log('fizzbuss')
+    }else if( number % 3 == 0){
+        console.log('buss')
+    }else if( number % 5 == 0){
+        console.log('fizz')
+    }else{
+        console.log(number)
+    }
+}
+
+Fizzbuzz(15)
+Fizzbuzz(3)
+Fizzbuzz(5)
+Fizzbuzz(11)
